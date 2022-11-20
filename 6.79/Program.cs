9f1cb06ae26eadca6dd8dae3f9bbc10a42cbdf2c
Yasuohasagi7rank <112807477+Yasuohasagi7rank@users.noreply.github.com>
2022-11-21 01:10:09 +0300

    Add files via upload

---
+++ 6.79/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _6._79
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введите число m: ");
+            int m = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите первый член g: ");
+            int g = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите знаменатель z: ");
+            int z = Convert.ToInt32(Console.ReadLine());
+            int i = 0;
+            while (i < m)
+            {
+                i = g * z;
+                g = g * z;
+                Console.WriteLine(g);
+            }
+            if (i == m)
+                Console.WriteLine("Геометрическая прогрессия");
+            else
+                Console.WriteLine("Не геометрическая прогрессия");
+        }
+    }
+}
